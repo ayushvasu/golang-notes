45dd2d5833faa17d1104f10a8545873f8b645da5
ayushvasu <ayushvasu98@gmail.com>
2024-06-22 16:46:18 +0530

    added mock test case

--- SimpleDB/db/sqlc/entry.sql.go
+++ SimpleDB/db/sqlc/entry.sql.go
@@ -20,7 +20,7 @@ INSERT INTO entries (
 
 type CreateEntryParams struct {
 	AccountID int64 `json:"account_id"`
-	Amount    int64       `json:"amount"`
+	Amount    int64         `json:"amount"`
 }
 
 func (q *Queries) CreateEntry(ctx context.Context, arg CreateEntryParams) (Entry, error) {
@@ -62,8 +62,8 @@ OFFSET $3
 
 type ListEntriesParams struct {
 	AccountID int64 `json:"account_id"`
-	Limit     int32       `json:"limit"`
-	Offset    int32       `json:"offset"`
+	Limit     int64         `json:"limit"`
+	Offset    int64         `json:"offset"`
 }
 
 func (q *Queries) ListEntries(ctx context.Context, arg ListEntriesParams) ([]Entry, error) {
@@ -84,6 +84,9 @@ func (q *Queries) ListEntries(ctx context.Context, arg ListEntriesParams) ([]Ent
 			return nil, err
 		}
 		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
 	}
 	if err := rows.Err(); err != nil {
 		return nil, err
--- SimpleDB/db/sqlc/store.go
+++ SimpleDB/db/sqlc/store.go
@@ -88,10 +88,37 @@ func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (Tr
 			return err
 		}
 
-		//todo : update accounts balance - as require locking
+		if arg.FromAccountId < arg.ToAccountId {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountId, -arg.Amount, arg.ToAccountId, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountId, arg.Amount, arg.FromAccountId, -arg.Amount)
+		}
 
-		return nil
+		return err
 	})
 
 	return result, err
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	return
 }
